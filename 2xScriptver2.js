b6c79d6ad53a7f8310ff98e438e8db1d4a3db65f
ILivS <sometimeyouthink@gmail.com>
2018-04-25 02:23:12 +0700

    Update 2xScriptver2.js

--- 2xScriptver2.js
+++ 2xScriptver2.js
@@ -6,8 +6,9 @@ var lossStreak = 0;
 //var cumulativeLoss=0;
 //var winStreak=0;
 var key=0;
+var _key=0;
 engine.on('game_starting', function(info) {
-		       
+		       console.log(key+" "+ _key);
 			engine.placeBet(currentBet*100,  Math.round(currentCashout*100));
 		
 
@@ -21,106 +22,83 @@ engine.on('game_crash', function(data) {
 	
 	if(engine.lastGamePlay()=='LOST'){
 
-		lossStreak++;
-	
-        
+		
+    if (_key==2)
+    {    
+      lossStreak++;
     currentBet=1;
    currentCashout=1;
-         
+         }  
+	else if (key ==1 ||  key ==2 || key ==3)
+  {
+_key++;
+currentBet*=2;
+currentCashout=2.2;
+
+
+  }
+  else 
+  {
+    lossStreak++;
+    currentBet=1;
+    currentCashout=1;
+  }
+  
 		
 		
 	}
 	else{
-		
-	
-			var thing = engine.getEngine();
+    var thing = engine.getEngine();
    var precr=thing.tableHistory[0].game_crash/100;
-   var precr1=thing.tableHistory[1].game_crash/100;
-
-  // console.log(precr+" " + precr1);
-if (precr>1.8 && key!=0 && precr1>2 )
-{
-   key=0;
-   currentBet=baseBet;
-   currentCashout=baseCashout;
-}
-  else  if (precr>2)
+    var precr1=thing.tableHistory[1].game_crash/100;
+   if (precr >2 )
    {
-   
-       if (lossStreak!=0)
+     if (lossStreak!=0)
        {
-       	key++;
-       	lossStreak=0;
+        key++;
+        lossStreak=0;
        }
-       
-       	
-       
-       if (key==1)
+       if (key!=0  && precr1>2 && precr>=2.2 ) 
        {
-       	currentBet=3;
-       	currentCashout=1.8;
-       }
+        key=0;
 
-       else if (key==2)
-       {
-       	currentBet=6;
-       	currentCashout=1.8;
        }
-       else if (key==3)
+        if (key==1)
        {
-       	currentBet=14;
-       	currentCashout=1.8;
+        currentBet=2;
+        currentCashout=2.2;
        }
-       else if (key==4)
+       else if (key==2 && _key==2)
        {
-       	currentBet=35;
-       	currentCashout=1.8;
-       } 
-       else if (key==5)
-       {
-       	currentBet=85;
-       	currentCashout=1.8;
+        currentBet=16;
+        currentCashout=2.2;
+        _key=0;
        }
-       else if (key==6)
+       else if (key==3 && _key==2)
        {
-       	currentBet=210;
-       	currentCashout=1.8;
+        currentBet=128;
+        currentCashout=2.2;
+        _key=0;
        }
-       else if (key==7)
-       {
-       	currentBet=525;
-       	currentCashout=1.8;
-       }
-       /*
-       else if (key==8)
-       {
-       	currentBet=256;
-       	currentCashout=1.8;
-       }
-       else if (key==9)
-       {
-       	currentBet=512;
-       	currentCashout=1.8;
-       }/*
-       else if (key==10)
-       {
-       	currentBet=1024;
-       	currentCashout=2.2;
-       } */ 
+       
+
        else 
        {
-       	currentBet=baseBet;
-       	currentCashout=baseCashout;
-       } 
+        currentBet=baseBet;
+currentCashout=baseCashout;
+       }
 
+
+       
    }
-   else if (precr<2 && lossStreak!=0)
+    else if (precr<2 && lossStreak!=0)
    {
 currentBet=1;
 currentCashout=1;
-		
+    
    }
-   
+		
+	
 
 }
 	
