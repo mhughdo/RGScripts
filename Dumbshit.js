ed3d9bb4b3096c172f823e450c43df29cf784aec
ILivS <sometimeyouthink@gmail.com>
2018-05-28 21:39:22 +0700

    Update Dumbshit.js

--- Dumbshit.js
+++ Dumbshit.js
@@ -1,72 +1,58 @@
+var baseBet=1;// Change thissssssssssssss
+var baseCashout = 2;
+var currentBet=baseBet ;
+var currentCashout = baseCashout;
+var lossStreak = 0;
+var lossTotal=0;
+//var cumulativeLoss=0;
+//var winStreak=0;
+var key=0;
 
-var timing=false ;
 
-
-engine.on('player_bet', function (data) { 
- var thing = engine.getEngine();
- //~~~~~~~~~~~~~~~~~~~~~~~Case 1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      for (let i=0;i<6;i++)
+engine.on('game_crash', function(data) {
+	
+	
+	
+	if(engine.lastGamePlay()=='LOST'){
+      if (		currentBet==512)
       {
-        if (thing.tableHistory[5].game_crash/100 <1.1)
-        {
-              for (let i=0;i<5;i++)
-      {
-         if (thing.tableHistory[i].game_crash/100 <1.1)
+		currentBet=baseBet;
+currentCashout=baseCashout;
+      }
+        
+    currentBet*=2;
+   currentCashout=2.2;
+         
+		
+		
+	}
+	else  if  (engine.lastGamePlay()=='WON' ) {
+		
+	
+
+   
+
+}
+
+var thing = engine.getEngine();
+         var winCnt=0;
+         var lossCnt=0;
+        // var act=false;
+         for (let i=0;i<10;i++)
          {
-                  timing=true;
+          if ( thing.tableHistory[i].game_crash/100>=1.97)
+          {
+            winCnt++;
+          }
+          else lossCnt++; 
+
          }
-      }
-        }
-      }
-       //~~~~~~~~~~~~~~~~~~~~~~~Case 2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      
-             if (thing.tableHistory[5].game_crash/100 >1.1)
-        {
-               for (let i=0;i<5;i++)
-               {
-                if (thing.tableHistory[i].game_crash/100 <2)
-        {
-          timing=false;
-                     break;
-        }
-        else {timing=true};
-                    
-               }
-        }
-      
-
-      /*
-       //~~~~~~~~~~~~~~~~~~~~~~~Case 3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-      for (let i=0;i<4;i++)
-      {
-if (thing.tableHistory[3].game_crash/100 <1.1)
-        {
-          check++;
-        }
-      for (let i=0;i<3;i++)
-      {
-            if (thing.tableHistory[i].game_crash/100 <1.1)
-            {
-              check++;
-            }
-      }
+         console.log("losscnt " + lossCnt +", wincnt : " + winCnt + ", key : " + key  +", bet " + currentBet );
+         if ( (winCnt>=lossCnt)  )
+    {
+			engine.placeBet(currentBet*100,  Math.round(currentCashout*100));
+		
     }
-      if (check==1)
-      {
-        timing=true;
-      } */ 
- if (timing==true) {
-  timing=false;
-  check=0;
-  check1=0;
-  var allin = parseInt(engine.getBalance()/100);
-  engine.placeBet(allin*100,  Math.round(1.09*100));
- }
-}); 
 
-
-
-
-
-
-
+	
+});
