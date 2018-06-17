3ca2c87347272b06d851f6909a63bc29534ea16f
ILivS <sometimeyouthink@gmail.com>
2018-06-17 21:32:03 +0700

    Update 2x Script.js

--- 2x Script.js
+++ 2x Script.js
@@ -1,54 +1,45 @@
-var baseBet=1;// Change thissssssssssssss
-var maxLosses=6;
-var skipGames=2;
+var baseBet=1;
 var baseCashout = 2;
 var currentBet=baseBet ;
 var currentCashout = baseCashout;
 var lossStreak = 0;
+var multi=[2,1.25,1.31,1.33,1.33,1.35,1.35,1.35,1.35,1.35];
 var play=true;
-var cnt=0;
-var multi=[1.08,1.25,1.28,1.31,1.33,1.35,1.35,1.35,1.35,1.35];
-engine.on('game_starting', function(info) {
-           
+var key=0;
 
-	
-	
-});
 
 engine.on('game_crash', function(data) {
 	
-    if(engine.lastGamePlay()=='NOT_PLAYED'&& lossStreak!=0)
-    {
-        cnt++;
-        if (cnt==skipGames)
-        {
-            cnt=0;
-            play=true;
+	if(engine.lastGamePlay()=='NOT_PLAYED'&&lossStreak!=0)
+		{
+            var thing = engine.getEngine();
+   var precr=thing.tableHistory[0].game_crash/100;
+  if (precr>=1.97)
+  {
+      play=true;
+  }
         }
-    }
-        
-
 	
 	if(engine.lastGamePlay()=='LOST'){
-        play=false;
- lossStreak++;
- if (lossStreak==maxLosses) engine.stop();
- currentBet*=4;
- currentCashout=multi[lossStreak];
 
-         
+		lossStreak++;
+	
+        
+    play=false;
+         currentBet*=4;
+         currentCashout=multi[lossStreak];
 		
 		
 	}
-    else if (engine.lastGamePlay()=="WON")
+	else if (engine.lastGamePlay()=="WON")
     {
         lossStreak=0;
         currentBet=baseBet;
         currentCashout=baseCashout;
         play=true;
     }
-    console.log(cnt+" "+lossStreak+" "+ currentBet +" " + currentCashout);
-    if(play)
-    engine.placeBet(currentBet*100,  Math.round(currentCashout*100));
+if(play)       
+engine.placeBet(currentBet*100,  Math.round(currentCashout*100));
+
 
 });
